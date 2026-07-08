4c0112d39b0c8a13eaa17107196a746e6bf28b60
Oleksandr Zazhytskyi <alexazaz@gmai.com>
2026-07-08 18:28:49 +0300

    Частина 2 — Запити до даних

--- queries/part2_queries.js
+++ queries/part2_queries.js
@@ -1,5 +1,6 @@
 use("spotify");
 
+// Частина 2 — Запити до даних
 // Завдання 1. Треки для вечірки
 
 tracks = db.tracks.find({
@@ -11,7 +12,7 @@ tracks = db.tracks.find({
     }
 });
 
-print('Треки для вечірки:', tracks);
+//print('Треки для вечірки:', tracks);
 
 // Завдання 2. Виконавці, у яких усі треки популярні
 
@@ -42,6 +43,55 @@ artists = db.tracks.aggregate([
     { $limit: 20 }
 ]);
 
-print('Виконавці:', artists);
+//print('Виконавці, у яких усі треки популярні:', artists);
+
+// Завдання 3. Нетипові треки
+
+tracks = db.tracks.aggregate([
+    {
+        $group: {
+            _id: "$track_genre",
+            avg_tempo: { $avg: "$audio_features.tempo" },
+            std_tempo: { $stdDevPop: "$audio_features.tempo" },
+            outlier_tracks: {
+                $push: {
+                    "avg_tempo": "$avg_tempo",
+                    "_id": "$_id",
+                    "track_name": "$track_name",
+                    "popularity": "$popularity",
+                    "artists": "$artists",
+                    "audio_features": {
+                        "tempo": "$audio_features.tempo"
+                    }
+                },
+            },
+        }
+    },
+    { $unwind: "$outlier_tracks" },
+    {
+        $project: {
+            _id: 0,
+            genre: "$_id",
+            avg_tempo: 1,
+            outlier_threshold: { $add: ["$avg_tempo", { $multiply: [2, "$std_tempo"] }] },
+            outlier_tracks: 1
+        }
+    },
+
+    { $match: { $expr: { $gt: ["$outlier_tracks.audio_features.tempo", "$outlier_threshold"] } } },
+]);
+
+//print('Нетипові треки:', tracks);
+
+// Завдання 4: Треки для фонової роботи
+
+tracks = db.tracks.find({
+    "audio_features.loudness": { $lt: -10 },
+    "audio_features.speechiness": { $lt: 0.1 },
+    "audio_features.instrumentalness": { $gt: 0.5 },
+    "explicit": { $eq: false },
+});
+
+//print('Треки для фонової роботи:', tracks);
 
 print("Finished");
